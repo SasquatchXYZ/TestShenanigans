faec50ab68045f6fb57a8df79d41753267a7c6fb
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-06-09 01:12:31 -0400

    Playing around with variables and logging

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use crate::chapter_18::test_vars::test_vars;
+use crate::chapter_18::test_var_logging::test_var_logging;
 
 mod chapter_18;
 
 fn main() {
-    test_vars()
+    test_var_logging()
 }
