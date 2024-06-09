0189abcf317f2377a5361d22d1f79d1533fdbb2e
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-06-08 23:50:10 -0400

    Test stdin

---
+++ src/chapter_18/test_stdin.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+pub fn test_stdin() {
+    println!("Please type something, or x to escape:");
+    let mut input_string = String::new();
+
+    while input_string.trim() != "x" {
+        input_string.clear();
+        io::stdin().read_line(&mut input_string).unwrap();
+        println!("You wrote {input_string}");
+    }
+    println!("See you later!");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use crate::chapter_18::test_stdin::test_stdin;
+
 mod chapter_18;
 
 fn main() {
-
+    test_stdin();
 }
