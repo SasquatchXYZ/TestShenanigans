0c343ebb3dbcf4b4a7627b1a754cedde9ef08e57
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-06-17 00:42:42 -0400

    Start Chapter 20

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-use crate::chapter_19::async_with_try_join::test_async_with_try_join;
+mod chapter_20;
 
-mod chapter_19;
-
-#[tokio::main]
-async fn main() {
-    test_async_with_try_join().await
-}
+fn main() {}
